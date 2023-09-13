fc02e925a7cdf4bf8eab3d2d4b9cc28ef16a3028
R V C <38222724+rvcgit@users.noreply.github.com>
2023-09-13 23:07:14 +0530

    Update dzenrofi.sh

--- bin/dzenrofi.sh
+++ bin/dzenrofi.sh
@@ -5,17 +5,17 @@ X=$(xrandr | grep '*' | uniq | awk '{print$1}' | cut -d 'x' -f1) #screen resolut
 Y=$(xrandr | grep '*' | uniq | awk '{print$1}' | cut -d 'x' -f2) #screen resolution ; Y
 XP="$(xdotool getmouselocation | awk '{print$1}' | sed 's/x://g')"
 YP="$(xdotool getmouselocation | awk '{print$2}' | sed 's/y://g')"
-if [[ "$YP" -lt "50" ]];then loc="1";else  # 50% of Y resolution 
+if [ "$YP" -lt "50" ];then loc="1";else  # 50% of Y resolution 
 	loc="7";fi &&
-if [[ $YP -le "50" ]];then anc="1";else
+if [ $YP -le "50" ];then anc="1";else
 	anc="7";fi &&
 
-if [[ $anc == "1" ]]; then pY=$(($YP+10));elif
-	[[ $anc == "7" ]]; then pY=$(YP-30);fi
+if [ $anc = "1" ]; then pY=$(($YP+10));elif
+	[ $anc = "7" ]; then pY=$($YP-20);fi
 
-if [[ $(($XP-$X)) -gt "$menu_width"  ]];then 
+if [ $(($X-$XP+10)) -lt "$menu_width"  ];then 
 	pX="$(($XP-$menu_width))";else 
-	pX="$(($XP-10))";fi ## x offset to align
+	pX="$(($XP+5))";fi ## x offset to align
 
 	rofi -show "drun" \
 		-modi "drun" \
