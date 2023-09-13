343baaedfb2385bfe626ffffad482ea27e34de8e
R V C <38222724+rvcgit@users.noreply.github.com>
2023-09-13 23:19:25 +0530

    Update dzentime.sh

--- bin/dzentime.sh
+++ bin/dzentime.sh
@@ -12,7 +12,7 @@ pX=$(($XP+10)) ## x offset to align
 
 (
 echo "Calendar"
-echo "$(cal -m | grep --color -EC6 "\b$(date +%e | sed 's/ //g')")"
+echo "$(cal -m | grep --color -EC6 "\b$(date +%e | sed 's/ //g')")" # ifcal -m returns blank, use just cal OR ncal -C
 echo "It's $(date '+%a %e %b')"
 ) | dzen2 -p -x $pX -y $pY -w "240" -l "9" -sa 'c' -ta 'c' -fn "RobotoMono Nerd Font:pixelsize=13:Bold:antialias=true"\
     -title-name 'Calendar' -e 'onstart=uncollapse;button1=exit;button4=exit;button5=exit'
