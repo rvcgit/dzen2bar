3d6261f02d6a11c055481dc69c90ec665df268df
rvcgit <38222724+rvcgit@users.noreply.github.com>
2023-09-24 12:08:15 +0530

    Update dzentime.sh

--- bin/dzentime.sh
+++ bin/dzentime.sh
@@ -16,5 +16,5 @@ echo "Calendar"
 echo "$(cal -m | grep --color -EC6 "\b$(date +%e | sed 's/ //g')")" # ifcal -m returns blank, use just cal OR ncal -C
 echo "It's $(date '+%a %e %b')"
 ) | dzen2 -p -x $pX -y $pY -w "240" -l "9" -sa 'c' -ta 'c' \
--bg '#134E4A' -fg '#CCFBF1' -fn "RobotoMono Nerd Font:pixelsize=13:Bold:antialias=true"\
+-bg '#134E4A' -fg '#CCFBF1' -fn "RobotoMono Nerd Font:pixelsize=13:Bold:antialias=true" \
 -title-name 'Calendar' -e 'onstart=uncollapse;button1=exit;button4=exit;button5=exit'
